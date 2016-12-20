36747f50b5c2316f4f157df80d88cec36a4a2a00
bengordon0123 <benjaminlouisgordon@gmail.com>
2016-12-20 14:49:37 -0500

    Add files via upload

---
+++ 12:20:16 Work.py
@@ -0,0 +1,13 @@
+print(100*1.1*1.1*1.1*1.1*1.1*1.1*1.1)
+savings = 100
+factor = 1.10
+result = 194.87171000000015
+desc = "compound interest"
+profitable = True
+x = False
+print(profitable)
+print(x)
+print(desc)
+
+
+      
